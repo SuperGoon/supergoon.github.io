5a1f89e3a1a88710b457c99e768c02be65c26f39
GiaiVTC@gmail.com <binhminh@gmail.com>
2020-06-29 18:39:10 +0700

    banco

--- Game2D-banco/index.js
+++ Game2D-banco/index.js
@@ -26,14 +26,10 @@ var ctx = canvas.getContext('2d')
 
 for (let i = 0; i < 8; i += 2) {
     for (let j = 1; j < 8; j += 2) {
-        ctx.beginPath()
         ctx.fillStyle = 'black';
-        ctx.rect(i * 100, j * 100, 100, 100)
+        ctx.fillRect(i * 100, j * 100, 100, 100)
         ctx.fill()
-        ctx.beginPath()
         ctx.fillStyle = 'black';
-        ctx.rect(j * 100, i * 100, 100, 100)
-        ctx.fill()
-        ctx.closePath()
+        ctx.fillRect(j * 100, i * 100, 100, 100)
     }
 }
