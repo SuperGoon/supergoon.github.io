369b9c5467e03032a5dd4b9217662c73251accb4
GiaiVTC@gmail.com <binhminh@gmail.com>
2020-09-25 08:01:05 +0700

    update task event listener

--- project/Task/main.js
+++ project/Task/main.js
@@ -1,23 +1,23 @@
-let result = document.getElementsByClassName('result')
+let result = $('.result')
 let string1 = '';
 let string2 = '';
 let string3 = '';
 let string4 = '';
 let number = null;
 
-const btn1 = document.getElementById('btn-1')
+const btn1 = $('#btn-1')
 
-btn1.addEventListener('click', getData)
-btn1.addEventListener('click', checkStringExist)
-btn1.addEventListener('click', shortenString)
-btn1.addEventListener('click', upperCaseFirstWord)
-btn1.addEventListener('click', repeatStringTask4)
-btn1.addEventListener('click', repeatStringTask5)
-btn1.addEventListener('click', repeatStringTask6)
-btn1.addEventListener('click', reverseString)
-btn1.addEventListener('click', testString)
+btn1.on('click', getData)
+btn1.on('click', checkStringExist)
+btn1.on('click', shortenString)
+btn1.on('click', upperCaseFirstWord)
+btn1.on('click', repeatStringTask4)
+btn1.on('click', repeatStringTask5)
+btn1.on('click', repeatStringTask6)
+btn1.on('click', reverseString)
+btn1.on('click', testString)
 
-btn1.addEventListener('click', consoleLog)
+btn1.on('click', consoleLog)
 
 function consoleLog() {
     console.log(string1.split(", "))
@@ -99,24 +99,26 @@ function testString() {
 
 let array1, array2, array3;
 
-const btn2 = document.getElementById('btn-2')
+const btn2 = $('#btn-2')
 
-btn2.addEventListener('click', getDataArray)
-btn2.addEventListener('click', minNumber)
-btn2.addEventListener('click', maxSecondNumber)
-btn2.addEventListener('click', sortName)
-btn2.addEventListener('click', sum)
-btn2.addEventListener('click', parameter)
-btn2.addEventListener('click', filterArray)
+btn2.on('click', getDataArray)
+btn2.on('click', minNumber)
+btn2.on('click', maxSecondNumber)
+btn2.on('click', sortName)
+btn2.on('click', sum)
+btn2.on('click', parameter)
+btn2.on('click', filterArray)
+    // $('#btn-2').click(() => maxSecondNumber())
+
 
 function getDataArray() {
-    array1 = document.getElementById('input-array-numbers').value.split(', ')
-    array2 = document.getElementById('input-array-name').value.split(', ')
-    array3 = document.getElementById('input-array-string').value.split(', ')
+    array1 = $('#input-array-numbers').val().split(', ')
+    array2 = $('#input-array-name').val().split(', ')
+    array3 = $('#input-array-string').val().split(', ')
 }
 
 function minNumber() {
-    result[8].innerHTML = array1.sort((a, b) => a - b)[0]
+    $('.result')[8].html(`${array1.sort((a, b) => a - b)[0]}`)
 }
 
 function maxSecondNumber() {
@@ -124,6 +126,7 @@ function maxSecondNumber() {
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] - arr[i - 1] < 0) {
             result[9].innerHTML = arr[i]
+            break;
         }
     }
 }
@@ -134,7 +137,7 @@ function sortName() {
     for (let i = arr.length - 1; i >= 0; i--) {
         arr1.push(arr[i])
     }
-    result[10].innerHTML = arr1
+    result[10].innerHTML = `[${arr1}]`
 }
 
 function sum() {
@@ -148,7 +151,7 @@ function sum() {
 }
 
 function parameter() {
-    result[12].innerHTML = array1.map(a => a % 2)
+    result[12].innerHTML = `[${array1.map(a => a % 2)}]`
 }
 
 function filterArray() {
