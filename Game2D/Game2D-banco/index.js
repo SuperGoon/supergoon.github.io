902dd6a18ce8d99ed6c039db21b3c3f6d7c78d15
supergoon <binhminh3426@gmail.com>
2020-10-17 10:47:29 +0700

    chess

--- Game2D/Game2D-banco/index.js
+++ Game2D/Game2D-banco/index.js
@@ -1,34 +1,33 @@
-var canvas = document.querySelector("#canvas")
-canvas.style.backgroundColor = "rgb(252, 179, 150)"
-canvas.width = 800
-canvas.height = 800
-canvas.style.border = '2px solid red'
-var ctx = canvas.getContext('2d')
-
-
-// for (let i = 0; i < 8; i += 2) {
-//     for (let j = 0; j < 8; j += 2) {
-//         ctx.beginPath()
-//         ctx.fillStyle = 'black';
-//         ctx.rect(i * 100, j * 100, 100, 100)
-//         ctx.fill()
-//     }
-// }
-// for (let i = 1; i < 8; i += 2) {
-//     for (let j = 1; j < 8; j += 2) {
-//         ctx.beginPath()
-//         ctx.fillStyle = 'black';
-//         ctx.rect(i * 100, j * 100, 100, 100)
-//         ctx.fill()
-//     }
-// }
-
+var canvas = document.querySelector("#canvas");
+canvas.style.backgroundColor = "rgb(252, 179, 150)";
+canvas.width = 1000;
+canvas.height = 1000;
+canvas.style.border = "2px solid red";
+var ctx = canvas.getContext("2d");
 
 for (let i = 0; i < 8; i += 2) {
     for (let j = 1; j < 8; j += 2) {
-        ctx.fillStyle = 'black';
-        ctx.fillRect(i * 100, j * 100, 100, 100);
-        ctx.fillRect(j * 100, i * 100, 100, 100);
-        ctx.fill()
+        ctx.fillStyle = "black";
+        ctx.fillRect(i * 100 + 100, j * 100 + 100, 100, 100);
+        ctx.fillRect(j * 100 + 100, i * 100 + 100, 100, 100);
+        ctx.fill();
     }
+}
+
+ctx.beginPath();
+ctxStroke = "blue";
+ctx.rect(100, 100, 800, 800);
+ctx.stroke();
+
+var text = ["A", "B", "C", "D", "E", "F", "G", "H"];
+var num = ["8", "7", "6", "5", "4", "3", "2", "1"];
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.font = "30px time news roman";
+
+for (let i = 0; i < 8; i++) {
+    ctx.fillText(text[i], 150 + i * 100, 50);
+    ctx.fillText(text[i], 150 + i * 100, 950);
+    ctx.fillText(num[i], 50, 150 + i * 100);
+    ctx.fillText(num[i], 950, 150 + i * 100);
 }
